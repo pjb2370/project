d92abaa7faeb1b22e27d43da240896757e0ce870
pjb2370 <dbwhd634@naver.com>
2022-08-17 18:35:51 +0900

    project2

--- src/ParkCompornents/Park.js
+++ src/ParkCompornents/Park.js
@@ -1,4 +1,4 @@
-import "../ParkCompornents/style.css";
+import style from "../ParkCompornents/style.module.css";
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Weather from "./Weather.js";
@@ -17,13 +17,45 @@ function Park() {
 
   return (
     <div>
-      <div className="blue" style={{ fontFamily: "alarm_clock" }}>
+      <div className={style.blue} style={{ fontFamily: "alarm_clock" }}>
         {time.format("YYYY-MM-DD")}
       </div>
-      <div className="neon green"> {time.format("HH-mm-ss")}</div>
-      <div className="Weather">
+      <div className={style.green}> {time.format("HH:mm:ss")}</div>
+      <div>
         <Weather />
       </div>
+      <section className={style.SNS}>
+        <div className={style.imaA}>
+          <a href="https://www.instagram.com/jjjb_1996/">
+            <img
+              src={require("./img/instagram.png")}
+              width="30"
+              height="30"
+              alt="kakaotalk"
+            />
+          </a>
+        </div>
+        <div className={style.imaB}>
+          <a href="https://open.kakao.com/o/sth18ewe">
+            <img
+              src={require("./img/kakaotalk.png")}
+              width="30"
+              height="30"
+              alt="kakaotalk"
+            />
+          </a>
+        </div>
+        <div className={style.imaC}>
+          <a href="https://www.facebook.com/profile.php?id=100003462018405">
+            <img
+              src={require("./img/facebook.png")}
+              width="30"
+              height="30"
+              alt="facebook"
+            />
+          </a>
+        </div>
+      </section>
     </div>
   );
 }
--- src/ParkCompornents/Weather.js
+++ src/ParkCompornents/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../ParkCompornents/style.css";
-import styled from "styled-components";
+import style from "../ParkCompornents/style.module.css";
 
 function Weather() {
   const [location, setLocation] = useState("");
@@ -25,9 +24,10 @@ function Weather() {
   };
 
   return (
-    <AppWrap>
-      <div className="appContentWrap">
+    <section className={style.AppWrap}>
+      <div className={style.appContentWrap}>
         <input
+          className={style.input}
           placeholder="도시를 입력하세요"
           value={location}
           onChange={(e) => setLocation(e.target.value)}
@@ -35,59 +35,17 @@ function Weather() {
           onKeyDown={searchWeather}
         />
         {Object.keys(result).length !== 0 && (
-          <ResultWrap>
-            <div className="city">{result.data.name}</div>
-            <div className="temperature">
+          <section className={style.ResultWrap}>
+            <div className={style.city}>{result.data.name}</div>
+            <div className={style.temperature}>
               {Math.round((result.data.main.temp - 273.15) * 10) / 10}°C
             </div>
-            <div className="sky">{result.data.weather[0].main}</div>
-          </ResultWrap>
+            <div className={style.sky}>{result.data.weather[0].main}</div>
+          </section>
         )}
       </div>
-    </AppWrap>
+    </section>
   );
 }
 
 export default Weather;
-
-const AppWrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  .appContentWrap {
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    padding: 20px;
-  }
-  input {
-    padding: 16px;
-    border: 2px black solid;
-    border-radius: 16px;
-  }
-`;
-
-const ResultWrap = styled.div`
-  margin-top: 60px;
-  border: 1px black solid;
-  padding: 10px;
-  border-radius: 8px;
-
-  .city {
-    font-size: 24px;
-    color: blue;
-  }
-  .temperature {
-    font-size: 60px;
-    margin-top: 8px;
-    text-shadow: 1px 0 rgb(230, 14, 14), 0 1px rgb(230, 14, 14),
-      1px 0 rgb(230, 14, 14), 0 -1px rgb(230, 14, 14);
-  }
-  .sky {
-    font-size: 20px;
-    text-align: right;
-    margin-top: 8px;
-    color: gray;
-  }
-`;
